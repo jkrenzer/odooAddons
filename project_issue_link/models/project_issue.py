078af64b22fe02226e2c13a176c1c9c093709629
Jörn Krenzer <joern.krenzer@gmx.de>
2018-04-17 18:31:55 +0200

    Added new module project_issue_link

---
+++ project_issue_link/models/project_issue.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class project_issue_link(models.Model):
+#     _name = 'project_issue_link.project_issue_link'
+
+#     name = fields.Char()
